37c7bb491125ad3c8ec839432bd884e3081b74a5
Michal Drewniak <drewniak@gmail.com>
2017-02-15 15:40:12 +0100

    Testing passing of origin as url param

--- src/bridge.js
+++ src/bridge.js
@@ -61,7 +61,7 @@ try {
     // epBridge was not found. In such case, we assume that the application is iframed within
     // WebPlayer and communication has to proceed via posting and receiving messages between windows.
     // TODO(michal): generalize hardcoded player.enplug.loc URL.
-    console.info('Initializing Web Development Player.')
+    console.info('Initializing Web Development Player.', window.location.href, window.location);
 
     epBridge = {
         send: (msg) => parent.postMessage(msg, window.location.origin.replace('apps', 'player'))
